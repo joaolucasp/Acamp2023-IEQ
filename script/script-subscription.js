237ddecaac4ecc7f99ed8397ad8bf222d375777b
João Lucas Pereira <lucaaspereeira@gmai.com>
2022-09-24 09:04:09 -0300

    forms completed

--- script/script-subscription.js
+++ script/script-subscription.js
@@ -1,116 +1,78 @@
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
-var current = 0;
+/*Funções que fazem verificações (não validações) de campos vazios*/ 
 
-$(".next").click(function(){
-    if(!validateForm()) return false;
-
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show();
-    current++;
-    
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({
-        'transform': 'scale('+scale+')',
-        'position': 'absolute'
-      });
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
-
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-    current--;
-
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
-
-$(".submit").click(function(){
-    if(!validateForm()) return false;
-	return false;
-})
-
-function validateForm() {
-    // This function deals with validation of the form fields
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("tab");
-    y = x[current].getElementsByTagName("input");
-    // A loop that checks every input field in the current tab:
-    for (i = 0; i < y.length; i++) {
-      // If a field is empty...
-      if (y[i].value == "") {
-        // add an "invalid" class to the field:
-        
-        y[i].className += " bg-danger";
-        // and set the current valid status to false:
-        valid = false;
-      }
-    }
-    // If the valid status is true, mark the step as finished and valid:
-    /*if (valid) {
-      document.getElementsByClassName("step")[currentTab].className += " finish";
-    }*/
-    return valid; // return the valid status
+function checkFields() {
+   /*Essa função deve ser responsável por checar os campos antes de avançar para a proxima etapa (ou quando clicar no botao de avançar*/ 
 }
+
 
 function removeInvalid(id) {
-    document.getElementById(id).className = ("bg-white");
+    /*Essa função deve ser responsável por remover a classe INVALID quando o usuário alterar o valor do campo*/
 }
+/*-----------------------------------------------------------------------*/
+
+/*Funções responsáveis por exibir um componente baseado em uma informação*/
+document.getElementById('igreja').onchange = function(){
+    if(igreja.value == 'outra-igreja') {
+        enableComponent('outra-igreja')
+        enableRequired('outra-igreja')
+    } else {
+        disableComponent('outra-igreja')
+        disableRequired('outra-igreja')
+    }
+};
+
+function disableComponent(id) {
+    if(!(document.getElementById(id).classList.contains('d-none'))){  
+        document.getElementById(id).classList.add('d-none');
+    }
+}
+
+function enableComponent(id) {
+    if(document.getElementById(id).classList.contains('d-none')){   
+        document.getElementById(id).classList.remove('d-none');
+    }
+}
+/*-----------------------------------------------------------------------*/
+
+/*Funções responsáveis por atribuir o atributo de required à elementos especificos*/
+function enableRequired(id) {
+    if(!document.getElementById(id).hasAttribute('required')){
+        document.getElementById(id).setAttribute('required','')
+    }
+}
+
+function disableRequired(id) {
+    if(document.getElementById(id).hasAttribute('required')){
+        document.getElementById(id).removeAttribute('required')
+    }
+}
+/*---------------------------------------------------------------------------------*/
+
+
+/*Funções responsáveis pela exibicação de alguns itens baseado na idade*/
+var birthDateElement = document.getElementById('data-nascimento');
+var dataToday = new Date();
+
+birthDateElement.onchange = function () {
+    var birthDate = new Date(String(birthDateElement.value));
+    var age = calculateAge(birthDate, dataToday);
+
+    if(age >= 18) {
+        disableComponent('nome-responsavel');
+        disableComponent('rg-responsavel')
+
+        disableRequired('nome-responsavel');
+        disableRequired('rg-responsavel');
+    } else {
+        enableComponent('nome-responsavel');
+        enableComponent('rg-responsavel') 
+
+        enableRequired('nome-responsavel');
+        enableRequired('rg-responsavel');
+    }
+}
+
+function calculateAge(birth, today){
+    return Math.floor(Math.ceil(Math.abs(birth.getTime() - today.getTime()) / (1000 * 3600 * 24)) / 365.25);
+}
+/*-----------------------------------------------------------------------*/
